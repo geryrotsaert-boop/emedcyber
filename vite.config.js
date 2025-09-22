9faa87967d78645b755b1dbc029928f276e8cc43
geryrotsaert-boop <geryrotsaert@gmail.com>
2025-09-22 11:59:53 +0200

    change vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: './src/main.jsx'
+      input: {
+        main: './index.html'  // ← POINT D'ENTRÉE = index.html
+      }
     }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  server: {
-    port: 3000,
-    open: true
   }
 })
