4377c476499fd9712a6d5f8274d13dba5da88942
geryrotsaert-boop <geryrotsaert@gmail.com>
2025-09-22 10:36:13 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
     rollupOptions: {
-      input: {
-        main: './src/main.jsx'
-      }
+      input: './src/main.jsx'
     }
-  },
-  server: {
-    port: 3000,
-    host: true,
   }
 })
